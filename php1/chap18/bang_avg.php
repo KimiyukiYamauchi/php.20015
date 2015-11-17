ba09ed5b102e5a2e4c05a4a289728fe5c77ef764
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2015-11-17 22:06:02 +0900

    chap19まで済み

--- php1/chap18/bang_avg.php
+++ php1/chap18/bang_avg.php
@@ -3,14 +3,22 @@
 $s = mysql_connect('localhost', 'yamauchi', '1234') or die('失敗です');
 print '成功しました<br>';
 mysql_select_db('db1');
-mysql_query("insert into tb1 values('K780', 'エスキュー花子', 25)");
-$re = mysql_query("select * from tb1");
+
+$q = <<<eot
+select bang,avg(uria)
+from tb
+where uria >= 50
+group by bang
+having avg(uria) >= 120
+order by avg(uria) desc;
+eot;
+
+$re = mysql_query($q);
 while($kekka = mysql_fetch_array($re)){
+	print '社員番号:';
 	print $kekka[0];
-	print ':';
+	print ' 売上平均:';
 	print $kekka[1];
-	print ':';
-	print $kekka[2];
 	print '<br>';
 }
 mysql_close($s);
--- php1/chap19/kantan_select.php
+++ php1/chap19/kantan_select.php
@@ -3,14 +3,14 @@
 $s = mysql_connect('localhost', 'yamauchi', '1234') or die('失敗です');
 print '成功しました<br>';
 mysql_select_db('db1');
-mysql_query("insert into tb1 values('K780', 'エスキュー花子', 25)");
-$re = mysql_query("select * from tb1");
+$re = mysql_query("select * from tbk order by bang");
 while($kekka = mysql_fetch_array($re)){
 	print $kekka[0];
-	print ':';
+	print ' : ';
 	print $kekka[1];
-	print ':';
+	print ' : ';
 	print $kekka[2];
 	print '<br>';
 }
 mysql_close($s);
+print "<br><a href='kantan.html'>トップメニューに戻ります</a>";
